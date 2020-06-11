6f0672b9685a3cf6caeb7cd1867131ee1fb80741
Erwan <rr.1.riou@gmail.com>
2020-06-11 15:36:17 +0200

    [Init] - initial commit, starting microservices structures

---
+++ posts/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+const posts = {}
+
+app.get("/posts", (req, res) => {
+res.send(posts)
+})
+
+app.post("/posts", (req, res) => {
+
+})
+
+app.listen(4000, () => {
+console.log("Listenning on 4000");
+})
