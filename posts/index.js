0947fe78097707776c1419f68d2e79e74ef15ef0
Erwan <rr.1.riou@gmail.com>
2020-10-01 21:41:50 +0200

    [Project] - update code

--- posts/index.js
+++ posts/index.js
@@ -19,7 +19,7 @@ app.post("/posts", async (req, res) => {
   const { title } = req.body
 
   posts[id] = { id, title }
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
@@ -35,6 +35,5 @@ app.post("/events", (req, res) => {
 })
 
 app.listen(4000, () => {
-  console.log("v20")
   console.log("Listenning on 4000")
 })
