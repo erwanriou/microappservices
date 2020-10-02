fa5d4e5072090fdd781c82346619a882228e26a7
Erwan <rr.1.riou@gmail.com>
2020-10-02 14:26:55 +0200

    [Project] - save apply refleted changes on skkafel

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-gnome-terminal --tab -- sh -c "cd client && npm start; exec bash"
-gnome-terminal --tab -- sh -c "cd comments && npm start; exec bash"
-gnome-terminal --tab -- sh -c "cd event-bus && npm start; exec bash"
-gnome-terminal --tab -- sh -c "cd moderation && npm start; exec bash"
-gnome-terminal --tab -- sh -c "cd posts && npm start; exec bash"
-gnome-terminal --tab -- sh -c "cd query && npm start; exec bash"
